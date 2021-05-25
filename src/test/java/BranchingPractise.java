8416fc6d29402157535b3a3944044708ed50ee94
Sarah ARSLAN <sarahkutahya@gmail.com>
2021-05-25 19:24:00 -0400

    shared on github

--- src/test/java/BranchingPractise.java
+++ src/test/java/BranchingPractise.java
@@ -1,5 +1,11 @@
 public class BranchingPractise {
     public static void main(String[] args) {
 
+        //create new project
+        //enable version control
+        //added .gitignore with .idea trget folder and .iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
+
     }
 }
